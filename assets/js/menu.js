b2451815b9bf7c53dab4fa9cc558108f97ef5f85
friendsqa <friendsqa@gmail.com>
2019-05-08 22:43:18 +0900

    Add menu animation

---
+++ assets/js/menu.js
@@ -0,0 +1,28 @@
+
+$(document).ready(function(){
+  var pathname = window.location.pathname;
+  if (pathname == "/") {
+    $("nav.navbar").css("background-color", "transparent");
+    // Modify dropdown 
+    $(".dropdown-content").css("background-color", "white");
+    $(".dropdown-content a").css("color", "black");
+  }
+});
+
+
+$(window).scroll(function(e) { 
+  var winLoc = $(window).scrollTop();
+
+  if (winLoc > 0) {
+    $("nav.navbar").css("background-color", "black");
+    // Modify dropdown 
+    $(".dropdown-content").css("background-color", "black");
+    $(".dropdown-content a").css("color", "white");
+  }
+  else if (winLoc == 0){
+    $("nav.navbar").css("background-color", "transparent");
+    // Modify dropdown 
+    $(".dropdown-content").css("background-color", "white");
+    $(".dropdown-content a").css("color", "black");
+  }
+});
